b436d3bb28336b69d45f525af4a75e796863fd28
Christina Loiacono <loiaconochristina@gmail.com>
2021-11-23 11:05:42 -0500

    we want to map through the productData betwen the render() and return()

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import { Component } from "react";
 import "./App.css";
+import data from './data/productData'
+
+console.log(data);
 
 class App extends Component {
-  // constructor(){
-  //   super();
+  constructor(){
+    super();
 
-  // }
+    this.state = {
+
+    }
+  }
 
   render(){
+    let productDataElArr = data.map((product)=>{
+      return (
+        <h3>{product.name}</h3>
+        
+      )
+    })
+
     return(
       <div>
         <h1>My Garage Sale Review</h1>
+        <div>
+          {productDataElArr}
+        </div>
       </div>
     )
   }
